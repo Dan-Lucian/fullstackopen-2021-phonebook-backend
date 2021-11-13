dd803a8ab5bdc0b1dae88056c1883bda73d9fd26
Dan-Lucian <danugaina1403@gmail.com>
2021-11-13 20:10:40 +0200

    add: ex 3.15
    * add delete person from db at delete request

--- index.js
+++ index.js
@@ -45,10 +45,11 @@ app.get('/api/persons/:id', (req, res) => {
 });
 
 app.delete('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id);
-  persons = persons.filter((person) => person.id !== id);
-
-  res.status(204).end();
+  Person.findByIdAndRemove(req.params.id)
+    .then(() => {
+      res.status(204).end();
+    })
+    .catch((err) => console.log(err));
 });
 
 app.post('/api/persons', (req, res) => {
@@ -68,6 +69,10 @@ app.post('/api/persons', (req, res) => {
     res.json(savedPerson);
   });
 });
+
+// app.put('/api/persons/:id', (req, res) => {
+
+// })
 
 let PORT = process.env.PORT;
 if (PORT == null || PORT == '') {
