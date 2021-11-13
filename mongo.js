ae8aa24eff8b326e0728b958f4e68f5f81316abe
Dan-Lucian <danugaina1403@gmail.com>
2021-11-13 11:57:45 +0200

    add: ex 3.12
    add: mongoose
    add: connection to mongo atlas

---
+++ mongo.js
@@ -0,0 +1,43 @@
+import mongoose from 'mongoose';
+
+if (process.argv.length < 3) {
+  console.log('Provide a password');
+  process.exit(1);
+}
+
+const password = process.argv[2];
+const url = `mongodb+srv://fullstack:${password}@phonebook.unj8n.mongodb.net/Phonebook?retryWrites=true&w=majority`;
+
+mongoose.connect(url);
+
+const personSchema = mongoose.Schema({
+  id: Number,
+  name: String,
+  number: String,
+});
+
+const Person = mongoose.model('Person', personSchema);
+
+if (process.argv.length === 3) {
+  Person.find({}).then((result) => {
+    console.log('Phonebook:');
+    result.forEach((person) => {
+      console.log(`${person.name} ${person.number}`);
+    });
+    mongoose.connection.close();
+  });
+}
+
+if (process.argv.length === 5) {
+  const name = process.argv[3];
+  const number = process.argv[4];
+  const person = new Person({
+    name,
+    number,
+  });
+
+  person.save().then((result) => {
+    console.log(`added ${name} number ${number} to phonebook`);
+    mongoose.connection.close();
+  });
+}
